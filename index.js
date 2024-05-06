4b50fcb02aae6026e0db4bb108b458362f432f03
eliud wainaina <wainainaeliud7@gmail.com>
2024-05-06 09:36:13 -0700

    lab done

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
-*/
+*/function reverse(word) {
+ 
+  return word.split("").reverse().join("");
+}
 
 /*
   Add written explanation of your solution here
@@ -16,8 +22,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
-
+  console.log("Expecting: true");
+console,log("=>", isPalindrome("a"))
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
